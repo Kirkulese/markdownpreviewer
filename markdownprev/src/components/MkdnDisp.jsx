ec6836d9169088b465232f5acf9ae1d8b74dde34
Kirkulese <44822130+Kirkulese@users.noreply.github.com>
2023-01-21 21:15:47 -0800

    everything working, needs styling

--- markdownprev/src/components/MkdnDisp.jsx
+++ markdownprev/src/components/MkdnDisp.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-
+import { marked } from 'marked';
 
 
 function MkdnDisp() {
 
   const markdown = useSelector((state) => state.markdown)
 
+  let parsed = marked.parse(markdown)
+
   return (
     <div>
-      <div id="preview">{markdown}</div>
+      <div id="preview" dangerouslySetInnerHTML={{__html: parsed}}>
+      </div>
+      
       
     </div>
   )
